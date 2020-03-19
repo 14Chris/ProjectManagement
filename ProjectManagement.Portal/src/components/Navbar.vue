4885849252fb66f387b044cb45ccc62cf2e77e84
14Chris <lenfant.chris@hotmail.fr>
2020-03-19 10:40:11 +0100

    Separate Navbar from Home component

---
+++ ProjectManagement.Portal/src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- Navbar -->
+    <b-navbar type="is-primary">
+      <template slot="start">
+        <b-navbar-item tag="router-link" to="/projects">Projects</b-navbar-item>
+      </template>
+      <template slot="end">
+        <b-dropdown aria-role="menu" class position="is-bottom-left">
+          <b-icon
+            type="is-white"
+            class="navbar-item"
+            icon="account"
+            size="is-medium"
+            slot="trigger"
+          ></b-icon>
+          <b-dropdown-item>
+            <router-link to="/profile">Profil</router-link>
+          </b-dropdown-item>
+          <b-dropdown-item value="settings">Settings</b-dropdown-item>
+          <b-dropdown-item @click.native.self="Logout()" value="logout" aria-role="menuitem">Logout</b-dropdown-item>
+        </b-dropdown>
+      </template>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    name:"Navbar",
+    data() {
+    return {
+      
+    };
+  },
+  mounted() {
+    // api.getData("Users/Session").then(resp => resp.json()); // Transform the data into json
+  },
+  methods: {
+    //To logout user of the app
+    Logout() {
+      localStorage.removeItem("userToken");
+      this.$router.push("/login");
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
